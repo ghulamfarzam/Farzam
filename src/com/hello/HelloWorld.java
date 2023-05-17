efef07819cc86b74aa9540bf5bbc61b33435f035
farzamali <gh.alifarzam@gmail.com>
2023-05-16 21:10:40 -0400

    Adding class Hello World

---
+++ src/com/hello/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.hello;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+//		String name = "Hello World this time from GIT and Git Hub";
+//		String[] names = "name, (9)";
+		
+		
+		System.out.println("Hello World this time from GIT and Git Hub");
+		
+		String hello = "hello world";
+		String[] sep = hello.split(" ");
+		String first = sep[0];
+		String second = sep[1];
+		System.out.println(first);
+		System.out.println(second);
+		
+		
+
+}
+}
+
+
+
